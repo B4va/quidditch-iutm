c5ae7709125859f53e9052f9c51387e45be504b7
Kevin Junck <junckkevin@outlook.com>
2021-04-13 16:07:52 +0200

    Ajout du Projet Serveur REST en C#

---
+++ quidditch-iutm-server/Quidditch-Server/Quidditch-Server/Models/Club.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quidditch_Server.Models
+{
+    public class Club
+    {
+        public string Name { get; private set; }
+
+        public Club(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ quidditch-iutm-server/Quidditch-Server/Quidditch-Server/Models/Team.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quidditch_Server.Models
+{
+    public class Team
+    {
+        public string Name { get; private set; }
+        public Image Logo { get; private set; }
+        public Club Club { get; private set; }
+        public Championship Championship { get; private set; }
+
+        public Team(string name, Image logo, Club club = null, Championship championship = null)
+        {
+            this.Name = name;
+            this.Logo = logo;
+            this.Club = club;
+            this.Championship = championship;
+        }
+    }
+}
