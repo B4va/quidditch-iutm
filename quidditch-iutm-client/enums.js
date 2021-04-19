27312ad78032fc5aaeb568002d6c38663e5c7740
Loïc STEINMETZ <loic.steinmetz2@gmail.com>
2021-04-19 14:54:02 +0200

    Enums MATCH_STATUS + GOLDEN_SNITCH

---
+++ quidditch-iutm-client/enums.js
@@ -0,0 +1,16 @@
+const MATCH_STATUS = {
+    SCHEDULED: 0,
+    IN_PROGRESS: 1,
+    FINISHED: 2
+}
+
+const GOLDEN_SNITCH = {
+    NONE: 0,
+    HOME: 1,
+    VISITOR: 2
+}
+
+module.exports = {
+    MATCH_STATUS,
+    GOLDEN_SNITCH
+}
