d1ca0b7c143651851fa4656294b0b6f07740d1c3
Loïc STEINMETZ <loic.steinmetz2@gmail.com>
2021-04-19 14:33:56 +0200

    Vue championship/:id

--- quidditch-iutm-client/controllers/championshipsController.js
+++ quidditch-iutm-client/controllers/championshipsController.js
@@ -55,20 +55,58 @@ const readLastChampionship = async (req, res) => {
             }
         ]
     };
+    renderCHampionship(res, championship);
+}
+
+const readChampionship = async (req, res) => {
+    const id = req.params.id;
+    // todo : données de test à remplacer
+    // const championship = await axios.get(`http://localhost/championships/${id}`);
+    const championship = {
+        id: 1,
+        year: 2000,
+        name: "Championnat de Poudelard 2000",
+        teams: [
+            {
+                id: 1,
+                name: "Gryffondor 2000",
+                logo: undefined,
+                points: 14
+            },
+            {
+                id: 2,
+                name: "Poufesouffle 2000",
+                logo: undefined,
+                points: 8
+            },
+            {
+                id: 3,
+                name: "Serdaigle 2000",
+                logo: undefined,
+                points: 10
+            },
+            {
+                id: 4,
+                name: "Serpentard 2000",
+                logo: undefined,
+                points: 12
+            }
+        ]
+    };
+    renderCHampionship(res, championship);
+}
+
+const readChampionshipMatches = async (req, res) => {
+
+}
+
+const renderCHampionship = (res, championship) => {
     res.render('championship', {
         title: 'Championnat en cours',
         championshipName: championship.name,
         teams: championship.teams.sort((a, b) => a.points - b.points).reverse(),
         id: championship.id
     });
-}
-
-const readChampionship = async (req, res) => {
-
-}
-
-const readChampionshipMatches = async (req, res) => {
-
 }
 
 module.exports = {
