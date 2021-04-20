2bfc2961b10d6920a2c874e7345f4e94d1f07072
Loïc STEINMETZ <loic.steinmetz2@gmail.com>
2021-04-20 19:28:22 +0200

    Vue clubs

--- quidditch-iutm-client/controllers/clubsController.js
+++ quidditch-iutm-client/controllers/clubsController.js
@@ -1,5 +1,40 @@
 const readClubs = async (req, res) => {
-
+    // todo : données de test à remplacer
+    // const championships = await axios.get('http://localhost/championships');
+    const clubs = [
+        {
+            "id": 1,
+            "name": "Griffondor",
+            "teams": [
+                {
+                    "id": 1,
+                    "name": "Griffondor 2000"
+                },
+                {
+                    "id": 2,
+                    "name": "Griffondor 2001"
+                },
+            ]
+        },
+        {
+            "id": 2,
+            "name": "Serpentard",
+            "teams": [
+                {
+                    "id": 3,
+                    "name": "Serpentard 2000"
+                },
+                {
+                    "id": 4,
+                    "name": "Serpentard 2001"
+                },
+            ]
+        },
+    ];
+    res.render('clubs', {
+        title: 'Clubs',
+        clubs: clubs
+    });
 }
 
 const readClub = async (req, res) => {
